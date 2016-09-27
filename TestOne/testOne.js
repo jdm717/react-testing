c22d869d8df1588d25eed5af36770e660d59a7a6
Jeff Murray <jeff@murray5.net>
2016-09-27 18:47:27 -0500

    Added bootstrap styling to buttons for TestOne and added React version. Also added timing for both.

--- TestOne/testOne.js
+++ TestOne/testOne.js
@@ -1,17 +1,32 @@
+function start_t1js() {
+	var startTime = getCurrentTime_t1js();
+	createTOneJS();
+	var endTime = getCurrentTime_t1js();
+
+	console.log("Render time: " + (endTime-startTime).toFixed(2) + "ms");
+	console.log("Render time: " + ((endTime-startTime) / 1000).toFixed(2) + "s");
+}
+
 
 function createTOneJS(){
 	var nodearr = [];
-	for(i = 0; i < 1000; i++){
+	for(i = 0; i < 50000; i++){
 		nodearr.push("button" + i);
 	}
-	debugger;
-	for(i = 0; i < 1000; i++){
-		var btn = document.createElement("input");
-		btn.setAttribute("type","button");
+
+	for(i = 0; i < 50000; i++){
+		var btn = document.createElement("button");
+		btn.className = "btn btn-primary";
+		btn.style.width = "100px";
+		btn.setAttribute("type", "button");
 		btn.setAttribute("value", nodearr[i]);
-		btn.setAttribute("name", nodearr[i]);
+		btn.innerHTML = nodearr[i];
 		var label = document.getElementById("screen");
 		label.appendChild(btn);
 	}
 
+}
+
+function getCurrentTime_t1js() {
+	return window.performance.now();
 }
--- TestOne/testOne.react.jsx
+++ TestOne/testOne.react.jsx
@@ -1,24 +1,51 @@
-var buttonScreen = React.createClass({
+var Button = ReactBootstrap.Button;
+
+function start_t1r() {
+	var nodearr = [];
+	for(i = 0; i < 50000; i++){
+		nodearr.push("button" + i);
+	}
+
+	var startTime = getCurrentTime_t1r();
+	React.render(<ButtonList data={nodearr} />, document.getElementById("content"));
+	var endTime = getCurrentTime_t1r();
+
+	console.log("Render time: " + (endTime-startTime).toFixed(2) + "ms");
+	console.log("Render time: " + ((endTime-startTime) / 1000).toFixed(2) + "s");
+}
+
+var ButtonList = React.createClass({
+	PropTypes: {
+		data: React.PropTypes.array.isRequired
+	},
+	render: function() {
+		var buttons = [];
+
+		for(var key in this.props.data) {
+			buttons.push(<CustomButton name={this.props.data[key]} key={key}/>);
+		}
+
+		return (
+			<div>
+				{buttons}
+			</div>
+		);
+	}
+})
+
+var CustomButton = React.createClass({
 	PropTypes: {
 		name: React.PropTypes.string.isRequired
 	},
-	render: function (){
+	render: function () {
 		return (
-			<div>
-				<p> {this.props.name} </p>;
-			</div>
+			<Button bsStyle="primary" style={{width: "100px"}}>
+				{this.props.name}
+			</Button>
 		);
 	}
 });
 
-React.render(<buttonScreen name={"Hello World"} />, document.getElementById("content"));
-
-// function createTOneRc(){
-// 	var label = document.getElementById("content");
-// 	React.render(<buttonScreen name="Hello World"/>, label);
-
-// }
-
-window.onload = function() {
-	debugger;
+function getCurrentTime_t1r() {
+	return window.performance.now();
 }
